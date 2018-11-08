abdc00320bdf3f02a5368cf67cdd5579e5819ef9
Dev_ES <anderson93@naver.com>
2018-11-08 21:12:13 +0900

    updated ui

--- app/src/main/java/com/owenism/ajouprint/SplashActivity.java
+++ app/src/main/java/com/owenism/ajouprint/SplashActivity.java
@@ -1,4 +1,21 @@
 package com.owenism.ajouprint;
 
-public class SplashActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(1200);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
 }
